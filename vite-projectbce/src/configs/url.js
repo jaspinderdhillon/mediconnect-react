cad91629deb925368dec3375120d6bb8316d697c
JASPINDER SINGH DHILLON <dhillonjpsingh7176@gmail.com>
2025-08-12 09:48:52 +0530

    Update backend API URL for deployed backend

--- vite-projectbce/src/configs/url.js
+++ vite-projectbce/src/configs/url.js
@@ -1,3 +1,5 @@
-export const serverl_url = "http://localhost:7176";
+// export const serverl_url = "http://localhost:7176";
 // export const serverl_url="https://bce-project-react-1.onrender.com";
 // export const serverl_url = "https://bce-project-react-1.onrender.com";
+export const serverl_url = "https://mediconnect-react.onrender.com";
+
